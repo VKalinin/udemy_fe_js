a94bfb91d845958088e1bcfb45d807304351e7ef
Влад Калинин <v.kalinin.m94@gmail.com>
2024-07-27 21:48:28 +0200

    second commit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const numberOfFilms = prompt('How many films have you watched already?', '');
+const numberOfFilms = +prompt('How many films have you watched already?', '');
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -8,12 +8,27 @@ const personalMovieDB = {
     privat : false
 };
 
-const a = prompt('One of the last viewed films?', ''),
-      b = prompt('Rate?', ''),
-      c = prompt('One of the last viewed films?', ''),
-      d = prompt('Rate?', '');
+for(let i = 0; i<2; i++){
+    const a = prompt('One of the last viewed films?', ''),
+        b = prompt('Rate?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a.length <= 50 && b.length <= 50 && a != '' && b != ''){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10){
+    console.log('You have saw so little number of films');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('You are a great watcher');
+} else if (personalMovieDB.count >= 30){
+    console.log('You are a cinephile');
+} else {
+    console.log('Something went wrong');
+}
 
 console.log(personalMovieDB);
